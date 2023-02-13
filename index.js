e1e89965500687fa009ece13e5fe14dc1d809714
7r-Rishi <7r.rushi@gmail.com>
2023-02-13 22:28:38 +0530

    PF

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// home typing
+var typed = new Typed(".typing", {
+    strings: ["Backed Developer", "Trader" , "Footballer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+// about me typing
+var typed = new Typed(".typing-2", {
+    strings: ["Backed Developer", "Trader" , "Footballer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
